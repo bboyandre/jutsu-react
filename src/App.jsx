88bf18aec58e84b39f5df9b8878b33bb344c1c3d
Andre Felipe <andre.felipe@novam3.com>
2022-03-06 00:02:44 -0300

    bankais aleatorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  
+  const [bankai, setBankai] = useState('Tensa Zangetsu')
+
+  const RandomElement = ()=> {
+    const arr = [
+      'Tensa Zangetsu',
+      'Senbonzakura Kageyoshi',
+      'Zanka no Tachi',
+      'Soo Zabimaru',
+      'Tekken Tachikaze',
+      'Konjiki Ashisogi Jizo',
+      'Daiguren Hyorinmaru',
+      'Minazuki',
+      'Mukojochu Muramasa',
+      'Ryumon Hozukimaru',
+      'Jakuho Raikoben',
+      'Kamishini no Yari',
+      'Kokujo Tengen Myo’o',
+      'Kinshara Butōdan',
+      'Katen Kyokotsu: Karamatsu Shinju',
+      'Kannonbiraki Benihime Aratame',
+      'Hakka no Togame',
+      'Raika Goen Kaku',
+      'Suzumushi Tsuishiki: Enma Kōrogi',
+      'Koko Gonryou Rikyu',
+      'Tekken Tachikaze'
+    ];
+    const randomElement = arr[Math.floor(Math.random() * arr.length)];
+    console.log(randomElement);
+    setBankai(randomElement)
+
+  }
+
   return (
     <div className="App">
-      <h1>
-        Hello
-      </h1>
+      <div className=''>
+        <p>
+          {bankai}
+        </p>
+      </div>
+      <button className='btn-no-jutsu' onClick={RandomElement}>Bankai</button>
     </div>
   )
 }
--- src/component/GlobalStyle.js
+++ src/component/GlobalStyle.js
@@ -2,9 +2,10 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        color: #332c36;
+        // color: #332c36;
+        color: #fff;
         padding: 0;
         margin: 0;
-        font-family: 'New Tegonin', serif;
+        font-family: 'New Tegonin', sans-serif;
     }
 `;
